e0a57b2ff58f0ec94f1fc89d3755b13b5d75622f
deepanshu garg <greekof022@gmail.com>
2024-08-08 18:14:11 +0530

    array

---
+++ array/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/*syntax and declaration 
+    start index of array -> [0]
+    declaration -> int arr[7] ->size 7
+    arr[3] =5 ; -> [3]indices 
+    arr[6] = 10; ->[3] indices  
+    [0,0,0,3,0,0,10];
+    array is collection of same data type.
+    array have same address 
+    ----------------------------
+    int arr[] = {1,3,4,9,8};
+    int arr[5] = {1,3,4,9,8};
+    ----------------------------
+    int arr[7];
+    arr[0]=7;
+    
+*/
+
+
+int main(){
+    int arr[5] = {1,3,4,9};
+    cout<<arr[5]<<endl;
+    
+}
